14bbe94573ebce8792fbd7819f22c863f9e9d838
John Hogenmiller <john@yourtech.us>
2019-07-12 18:41:15 -0400

    hello world or name

--- learntestgo/hello/hello.go
+++ learntestgo/hello/hello.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
+
 	fmt.Println(Hello("world"))
 }
