c3b516f79fb074739d6623b861fbb32e63ef4595
John Hogenmiller <john@yourtech.us>
2019-07-12 18:49:36 -0400

    add spanish prefix

--- learntestgo/hello/hello_test.go
+++ learntestgo/hello/hello_test.go
@@ -13,7 +13,7 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
@@ -22,10 +22,15 @@ func TestHello(t *testing.T) {
 
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
