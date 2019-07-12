046222fd6b02442632d8d776797bd841b2a512fd
John Hogenmiller <john@yourtech.us>
2019-07-12 18:58:17 -0400

    hello adding prefix and switch statements

--- learntestgo/hello/hello_test.go
+++ learntestgo/hello/hello_test.go
@@ -33,4 +33,10 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francois", "French")
+		want := "Bonjour, Francois"
+		assertCorrectMessage(t, got, want)
+	})
 }
