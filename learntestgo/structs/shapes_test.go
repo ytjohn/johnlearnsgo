eb51ec989a3350c061c3ae4051aded8c6dbdd5de
John Hogenmiller <john@yourtech.us>
2019-08-15 11:22:11 -0400

    tdd: structs methods interfaces

--- learntestgo/structs/shapes_test.go
+++ learntestgo/structs/shapes_test.go
@@ -2,9 +2,9 @@ package structs
 
 import "testing"
 
-func TestPerimiter(t *testing.T) {
+func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimiter(rectangle)
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -13,11 +13,20 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+
 }
