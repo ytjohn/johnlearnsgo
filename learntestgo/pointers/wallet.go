4d5382943768232f8dbbb0137dd588f9d1081a90
John Hogenmiller <john@yourtech.us>
2019-08-28 10:36:08 -0400

    tdd: finished pointers

---
+++ learntestgo/pointers/wallet.go
@@ -0,0 +1,38 @@
+package pointers
+
+import "fmt"
+import "errors"
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Deposit(amount Bitcoin){
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string{
+	return fmt.Sprintf("%d BTC", b)
+}
+
